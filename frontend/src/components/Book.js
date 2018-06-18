fe6ab76df558cde743887e6c10468d7ff6ea13c8
Karthik Rao <karthikvrao88@gmail.com>
2018-06-18 22:26:46 +0530

    feat: Remove dummy books data and connect BookShelf component to redux store

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -6,9 +6,6 @@ const Book = ({ book }) => {
     <Link to={`/books/${book.id}`}>
       <div className="book">
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">
-          {book.authors && book.authors.join(", ")}
-        </div>
         <div className="book-pages">{book.pages} pages</div>
       </div>
     </Link>
--- frontend/src/components/BookShelf.js
+++ frontend/src/components/BookShelf.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import BooksGrid from './BooksGrid';
 
 class BookShelf extends Component {
   render() {
-    //dummy data
-    const books = [
-      { id: 1, title: 'sometitle1', authors: ['someauthor1', 'someauthor2'], pages: 80 },
-      { id: 2, title: 'sometitle2', authors: ['someauthor2', 'someauthor3'], pages: 820 },
-      { id: 3, title: 'sometitle3', authors: ['someauthor3', 'someauthor4'], pages: 50 },
-      { id: 4, title: 'sometitle4', authors: ['someauthor4', 'someauthor5'], pages: 97 },
-      { id: 5, title: 'sometitle5', authors: ['someauthor5', 'someauthor6', 'someauthor7'], pages: 999 },
-      { id: 6, title: 'sometitle5', authors: ['someauthor5', 'someauthor6', 'someauthor7'], pages: 999 },
-      { id: 7, title: 'sometitle5', authors: ['someauthor5', 'someauthor6', 'someauthor7'], pages: 999 },
-      { id: 8, title: 'sometitle5', authors: ['someauthor5', 'someauthor6', 'someauthor7'], pages: 999 }
-    ];
-    return (
+    const { books } = this.props;
+    const bookList = Object.values(books);
+    const noBooks = bookList === undefined || bookList.length === 0;
 
+    return (
       <div>
         <div className="actions-bar">
           <h3><Link className="link-to-home" to="/">Home</Link></h3>
-          {/* <h2>Search</h2> */}
           <div className="action-buttons">
             <Link to="/authors/create">Add author</Link>
             <Link to="/books/create">Add book</Link>
           </div>
         </div>
-        <BooksGrid books={books} />
+        {noBooks ?
+          <div className="noBooksFound">
+            <p><strong>No books found in shelf</strong></p>
+          </div> :
+          <BooksGrid books={bookList} />
+        }
       </div>
     );
   }
 }
 
-export default BookShelf;
+const mapStateToProps = ({ books }) => ({ books: books.byId });
+
+export default connect(mapStateToProps)(BookShelf);
